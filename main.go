4a2aa76784da5fe63b0bd388d7a84d345198c4c3
Sarah Ramadhina Faza <sarah0ramadhina@gmail.com>
2023-10-31 13:31:43 +0000

    feat: add duration

--- main.go
+++ main.go
@@ -4,7 +4,9 @@ import (
 	"flag"
 	"github.com/sarahrmhdn/golang-stresstester/library"
 	"log"
+	"os"
 	"sync"
+	"time"
 )
 
 func main() {
@@ -13,11 +15,13 @@ func main() {
 		threads int
 		mode    string
 		wg      sync.WaitGroup
+		howlong int
 	)
 
 	// Define flags
 	flag.StringVar(&url, "url", "https://example.com", "Specify target url (with http/https)")
 	flag.IntVar(&threads, "threads", 10, "Specify number of threads to attack")
+	flag.IntVar(&howlong, "howlong", 3, "How long attack will be running (in seconds). -1 means forever")
 	flag.StringVar(&mode, "mode", "normal", "Specify mode ('normal', 'wordpress')")
 
 	// Parse the flags
@@ -27,6 +31,18 @@ func main() {
 		Mode: mode,
 	}
 	// Use the values from flags
+
+	// Simulate a main task that could take a long time
+	if howlong != -1 {
+		go func() {
+			log.Printf("This will run maximum %d seconds\n", howlong)
+			time.Sleep(time.Duration(howlong) * time.Second)
+			os.Exit(0)
+		}()
+	} else {
+		log.Println("This job will run forever")
+	}
+
 	log.Printf("Request received with URL %s and threads %d and mode %s\n", url, threads, mode)
 	wg.Add(threads)
 	for i := 1; i <= threads; i++ {
